689d85685af3893c06ea83438e149f4c511dccf9
pizza-pizza-pie <126295116+marcorios-g@users.noreply.github.com>
2023-08-30 12:18:15 -0600

    nav scroll behaviour
    pending:
    - nav dropdown z-index

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 'use strict';
 
+// scroll behaviour
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener('scroll', function () {
+    const currentScroll = this.pageYOffset;
+
+    if (currentScroll <= 0) {
+        body.classList.remove('scroll-up');
+    }
+
+    if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+        body.classList.remove('scroll-up');
+        body.classList.add('scroll-down');
+    }
+
+    if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
+        body.classList.remove('scroll-down');
+        body.classList.add('scroll-up');
+    }
+
+    lastScroll = currentScroll;
+})
+
+// burger menu
 const burgerMenu = document.querySelector('.burger-menu');
 const burgerBar1 = document.querySelector('.bar-1');
 const burgerBar2 = document.querySelector('.bar-2');
@@ -7,8 +32,6 @@ const burgerBar3 = document.querySelector('.bar-3');
 const dropdownMenu = document.querySelector('.dropdown');
 
 burgerMenu.addEventListener('click', function () {
-    console.log('burger clicked');
-
     burgerBar1.classList.toggle('burger-bar-top-anim');
     
     burgerBar2.classList.toggle('burger-bar-mid-anim');
